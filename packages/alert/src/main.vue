8202cb35bcabeac3e154cb0075d5c0d3c92b1ea8
xuho <xuhao17@tal.com>
2021-04-08 19:55:44 +0800

    alert

--- packages/alert/src/main.vue
+++ packages/alert/src/main.vue
@@ -1,16 +1,35 @@
 <template>
   <transition name="el-alert-fade">
+
+    <!-- 
+      typeClass：根据传入的type控制样式
+      center: 根据传入的center控制内容是否居中
+      role：无障碍网页应用,读屏软件会用到
+     -->
     <div
       class="el-alert"
       :class="[typeClass, center ? 'is-center' : '', 'is-' + effect]"
       v-show="visible"
       role="alert"
     >
+      <!-- 
+        showIcon: 控制是否显示icon
+        iconClass: 根据传入的type动态控制icon的样式
+        isBigIcon： 如果传入了description（辅助性文字）或者 插槽内传入了内容 则显示大icon
+$slots.default ： 获取所有匿名插槽分发的内容
+       -->
       <i class="el-alert__icon" :class="[ iconClass, isBigIcon ]" v-if="showIcon"></i>
       <div class="el-alert__content">
+        <!-- 如果传入了description（辅助性文字）或者 传入了匿名插槽 则显示加粗title -->
         <span class="el-alert__title" :class="[ isBoldTitle ]" v-if="title || $slots.title">
+        <!-- 传入插槽内容则显示插槽内容 未传入默认显示slot内的内容description -->
           <slot name="title">{{ title }}</slot>
-        </span>
+        </span>  
+        <!-- 
+          closable: 是否显示关闭按钮，默认显示
+          closeText： 关闭按钮自定义文字，无显示X有显示文字
+         -->
+       
         <p class="el-alert__description" v-if="$slots.default && !description"><slot></slot></p>
         <p class="el-alert__description" v-if="description && !$slots.default">{{ description }}</p>
         <i class="el-alert__closebtn" :class="{ 'is-customed': closeText !== '', 'el-icon-close': closeText === '' }" v-show="closable" @click="close()">{{closeText}}</i>
@@ -37,6 +56,7 @@
         type: String,
         default: ''
       },
+      // 接收主题 computed里会根据不同type返回对应的样式
       type: {
         type: String,
         default: 'info'
@@ -45,6 +65,7 @@
         type: Boolean,
         default: true
       },
+      // 关闭按钮自定义文字
       closeText: {
         type: String,
         default: ''
